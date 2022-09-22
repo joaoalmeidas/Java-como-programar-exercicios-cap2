176551b805e86b516a55e8ce5c58190557f946e9
Joao Almeida <joao182cx@gmail.com>
2022-09-22 20:08:59 -0300

    Resolução do exercício 2.18

---
+++ 2.18 Exibindo formas com asteriscos/src/formaseasteriscos/Formas.java
@@ -0,0 +1,19 @@
+package formaseasteriscos;
+
+public class Formas {
+
+	public static void main(String[] args) {
+		
+		System.out.print(	"*********    ***      *        *\n"+
+							"*       *   *   *    ***      * *\n"+
+							"*       *  *     *  *****    *   *\n"+
+							"*       *  *     *    *     *     *\n"+
+							"*       *  *     *    *    *       *\n"+
+							"*       *  *     *    *     *     *\n"+
+							"*       *  *     *    *      *   *\n"+
+							"*       *   *   *     *       * *\n"+
+							"*********    ***      *        *\n");
+
+	}
+
+}
