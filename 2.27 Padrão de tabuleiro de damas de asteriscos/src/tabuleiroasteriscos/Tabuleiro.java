700c364c652e492c7be8541e519435c81e4bdc11
Joao Almeida <joao182cx@gmail.com>
2022-09-23 19:50:10 -0300

    Resolução do exercício 2.27

---
+++ 2.27 Padrão de tabuleiro de damas de asteriscos/src/tabuleiroasteriscos/Tabuleiro.java
@@ -0,0 +1,20 @@
+package tabuleiroasteriscos;
+
+public class Tabuleiro {
+
+	public static void main(String[] args) {
+		
+		//2.27 (Padrão de tabuleiro de damas de asteriscos) Escreva um aplicativo que exiba um padrão de tabuleiro de damas, como mostrado a seguir:
+		
+		System.out.print(	"* * * * * * * * \n"
+						+ 	" * * * * * * * *\n"
+						+	"* * * * * * * * \n"
+						+ 	" * * * * * * * *\n"
+						+	"* * * * * * * * \n"
+						+ 	" * * * * * * * *\n"
+						+	"* * * * * * * * \n"
+						+ 	" * * * * * * * *\n");
+
+	}
+
+}
