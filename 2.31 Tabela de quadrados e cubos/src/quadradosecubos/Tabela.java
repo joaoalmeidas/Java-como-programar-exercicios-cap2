8fd90b8dc020893d7bdb4a8a0e8be148b71ed978
Joao Almeida <joao182cx@gmail.com>
2022-09-24 14:07:35 -0300

    Resolução do exercício 2.31

---
+++ 2.31 Tabela de quadrados e cubos/src/quadradosecubos/Tabela.java
@@ -0,0 +1,66 @@
+package quadradosecubos;
+
+public class Tabela {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 2.31 (Tabela de quadrados e cubos) Utilizando apenas as técnicas de programação que aprendeu neste capítulo, escreva um aplicativo que
+			calcule os quadrados e cubos dos números de 0 a 10 e imprima os valores resultantes em formato de tabela como a seguir:
+			number square cube
+			0 0 0
+			1 1 1
+			2 4 8
+			3 9 27
+			4 16 64
+			5 25 125
+			6 36 216
+			7 49 343
+			8 64 512
+			9 81 729
+			10 100 1000
+		 */
+		
+		int num;
+		
+		System.out.println("Tabela de quadrados e cubos de 1 a 10.\n");
+		
+		System.out.printf("Número\tQuadrado\tCubo%n");
+		
+		num = 0;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 1;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 2;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 3;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 4;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 5;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 6;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 7;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 8;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 9;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		num = 10;
+		System.out.printf("%d\t%d\t\t%d%n", num, (num*num), (num*num*num));
+		
+		
+	}
+
+}
